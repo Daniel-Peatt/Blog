b1f6941ae3c706b294aac1fb7e9ac00dd57efffa
Daniel Peatt <danielpeatt@gmail.com>
2024-09-12 18:37:38 -0400

    Add placeholder blog post

--- index.js
+++ index.js
@@ -7,6 +7,30 @@ const app = express();
 // Creating an array of objects to store the blog post information
 let blogPost = [];
 
+// Fill blogPost with Example
+blogPost.push({
+    title: "Dachshund", 
+    content: `Dachshund, dog breed of hound and terrier ancestry developed in Germany to pursue badgers into their burrows. The Dachshund is a long-bodied, characteristically lively dog with a deep chest, short legs,
+     tapering muzzle, and long ears. Usually reddish brown or black-and-tan, it is bred in two sizes—standard and miniature—and in three coat types—smooth, longhaired, and wirehaired. The standard Dachshund stands about 8 
+     to 9 inches (20 to 23 cm) tall at the withers and weighs 16 to 32 pounds (7 to 14.5 kg). The miniature is shorter and weighs no more than 11 pounds (5 kg).`});
+blogPost.push({
+    title: "Ireland", 
+    content: `The magnificent scenery of Irelands Atlantic coastline faces a 2,000-mile- (3,200-km-) wide expanse of ocean, and its geographic isolation has helped it to
+     develop a rich heritage of culture and tradition that was linked initially to the Gaelic language. Washed by abundant rain, the countrys pervasive grasslands create a green-hued landscape that is responsible 
+     for the popular sobriquet Emerald Isle. Ireland is also renowned for its wealth of folklore, from tales of tiny leprechauns with hidden pots of gold to that of the patron saint, Patrick, with his legendary ridding 
+     the island of snakes and his reputed use of the three-leaved shamrock as a symbol for the Christian Trinity. But while many may think of Ireland as an enchanted land, the republic has been beset with perennial 
+     concerns—emigration, cultural and political identity, and relations with Northern Ireland comprising the 6 of Irelands 32 counties within the province of Ulster that remain part of the United Kingdom. At the
+     beginning of the 21st century, Irelands long-standing economic problems were abating, owing to its diverse export-driven economy, but calamity struck again in 2008 when a new financial and economic crisis befell 
+     the country, culminating in a very costly bailout of the Irish economy by the European Union EU and the International Monetary Fund.`});
+blogPost.push({
+    title: "Florida Manatee", 
+    content: `The Florida manatee, a subspecies of the West Indian manatee, is a large, slow-moving marine mammal with an elongated, round body and paddle-shaped flippers and tail. Manatees are herbivores, 
+    feeding solely on seagrass, algae and other vegetation in freshwater and estuarine systems in the southeastern United States. Florida manatees can be found as far west as Texas and as far north as Massachusetts 
+    during summer months, but during the winter, manatees congregate in Florida, as they require warm-water habitats to survive. Abundance of the subspecies has increased over the last 30 years, which prompted the 
+    U.S. Fish and Wildlife Service (FWS) to downlist the West Indian manatee from endangered to threatened in 2017. However, due to their slow speed and relatively high buoyancy, manatees are often struck by vessels, 
+    which is the primary cause of human-related deaths of the species. Additionally, manatees continue to be threatened by loss of warm-water habitat and periodic die-offs from red tides and unusually cold weather events. 
+    Florida manatees are managed jointly by both FWS and the Florida Fish and Wildlife Conservation Commission (FWC).`});
+
 // Setting the static files file path to start at public
 app.use(express.static("public"));
 
